aca9f0c88e8393ac3c461c70f5ef97881d295624
Davi Barreto <daviarturdesa686@gmail.com>
2024-03-18 13:11:04 -0300

    Switch nativecn_ui

---
+++ src/components/Switch.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Switch as NativeSwitch} from 'react-native';
+
+import {colors} from '../styles/colors';
+
+function Switch({
+  ...props
+}: React.ComponentPropsWithoutRef<typeof NativeSwitch>) {
+  return (
+    <NativeSwitch
+      className="h-6 w-6"
+      trackColor={{
+        true: colors.green[400],
+        false: colors.gray[500],
+      }}
+      thumbColor={colors.green[500]}
+      {...props}
+    />
+  );
+}
+
+export {Switch};
